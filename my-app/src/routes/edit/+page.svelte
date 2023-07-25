d98a54077441f9ab5504e7f6318ef911aefd8099
Derrick <derrick.ko912@gmail.com>
2023-07-25 16:30:23 -0700

    simple text input box

--- my-app/src/routes/edit/+page.svelte
+++ my-app/src/routes/edit/+page.svelte
@@ -1,8 +1,25 @@
 <svelte:head>
-	<title>Edit</title>
-	<meta name="description" content="About this app" />
+  <title>Edit</title>
+  <meta name="description" content="About this app" />
 </svelte:head>
+  
+<script>
+  let inputValue = ""; // Initialize the variable to store the input value
 
-<div>
-	
-</div>
+  const handleSubmit = () => {
+    // Handle the submission here, e.g., send the value to a server
+    alert("Submitted: " + inputValue);
+  };
+  import TextEditor from "./TextEditor.svelte"; // Import the TextEditor component
+</script>
+
+<TextEditor bind:inputValue /> <!-- Bind the inputValue to the TextEditor's inputValue -->
+
+<h2>Standalone Input Box:</h2>
+<input
+  type="text"
+  placeholder="Enter your text here"
+  bind:value={inputValue}
+/>
+
+<button on:click={handleSubmit} style="margin-top: 10px;">Submit</button>
